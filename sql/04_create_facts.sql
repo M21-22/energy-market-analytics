ba519e0b188514caaf54853464f43239eaaec5d7
murad_aghayev <murad_aghayev@epam.com>
2026-09-13 03:27:10 +0400

    create facts

---
+++ sql/04_create_facts.sql
@@ -0,0 +1,75 @@
+/* =========================================================
+   FACT TABLES
+   ========================================================= */
+
+
+/* -------------------------
+   FactClimate
+   Grain: State × Month
+   ------------------------- */
+
+CREATE TABLE dw.FactClimate
+(
+    date_key             INT NOT NULL,
+    state_key            INT NOT NULL,
+
+    avg_temperature      FLOAT NULL,
+    cooling_degree_days  FLOAT NULL,
+    heating_degree_days  FLOAT NULL,
+    precipitation        FLOAT NULL
+)
+WITH
+(
+    DISTRIBUTION = ROUND_ROBIN,
+    HEAP
+);
+GO
+
+
+/* -------------------------
+   FactRetailElectricity
+   Grain: State × Month × Sector
+   ------------------------- */
+
+CREATE TABLE dw.FactRetailElectricity
+(
+    date_key                    INT NOT NULL,
+    state_key                   INT NOT NULL,
+    sector_key                  INT NOT NULL,
+
+    revenue_thousand_dollars    FLOAT NULL,
+    sales_mwh                   FLOAT NULL,
+    customer_count              FLOAT NULL,
+    avg_price_cents_per_kwh     FLOAT NULL,
+    data_status                 VARCHAR(20) NULL
+)
+WITH
+(
+    DISTRIBUTION = ROUND_ROBIN,
+    HEAP
+);
+GO
+
+
+/* -------------------------
+   FactElectricityGeneration
+   Grain:
+   State × Month × Producer Type × Energy Source
+   ------------------------- */
+
+CREATE TABLE dw.FactElectricityGeneration
+(
+    date_key            INT NOT NULL,
+    state_key           INT NOT NULL,
+    producer_type_key   INT NOT NULL,
+    energy_source_key   INT NOT NULL,
+
+    generation_mwh      FLOAT NULL,
+    data_status         VARCHAR(20) NULL
+)
+WITH
+(
+    DISTRIBUTION = ROUND_ROBIN,
+    HEAP
+);
+GO
