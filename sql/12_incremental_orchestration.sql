5c86b77361b3a0b6307f578fcceb144b3064613a
murad_aghayev <murad_aghayev@epam.com>
2026-09-20 00:09:57 +0400

    added validation to orchestration

--- sql/12_incremental_orchestration.sql
+++ sql/12_incremental_orchestration.sql
@@ -1,7 +1,10 @@
-CREATE PROCEDURE dw.usp_LoadWarehouseIncremental
+CREATE OR ALTER PROCEDURE dw.usp_LoadWarehouseIncremental
 AS
 BEGIN
     SET NOCOUNT ON;
+
+    -- Block warehouse loading if staging data fails validation.
+    EXEC dw.usp_ValidateStaging;
 
     EXEC dw.usp_LoadRetailIncremental;
     EXEC dw.usp_LoadGenerationIncremental;
